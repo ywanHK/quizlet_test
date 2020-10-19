d2f14a96f024c215fefbb26682fc551dbeab5e4f
ywanHK <39464944+ywanHK@users.noreply.github.com>
2020-10-19 20:50:54 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 :: gcc api.c %*
 echo %1 | findstr /r "dll" >nul 2>&1 && (
-	gcc -shared *.c -o %*
+	gcc -shared *.c -o %* && echo BUILD SUCCESSFUL || echo BUILD NOT SUCCESSFUL
 ) || (
-	gcc *.c %*
+	gcc *.c %* && echo BUILD SUCCESSFUL || echo BUILD NOT SUCCESSFUL
 )
