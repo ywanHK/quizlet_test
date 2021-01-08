a8a7f4246bbc23cdbf809c3a652bb23e0c9791bc
ywanHK <39464944+ywanHK@users.noreply.github.com>
2021-01-08 19:48:53 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 @echo off
-:: gcc api.c %*
-echo %1 | findstr /r "dll" >nul 2>&1 && (
-	gcc -shared *.c -o %* && echo BUILD SUCCESSFUL || echo BUILD NOT SUCCESSFUL
-) || (
-	gcc *.c %* && echo BUILD SUCCESSFUL || echo BUILD NOT SUCCESSFUL
+if "%1"=="-s" (
+	del *.o /s /q
 )
+mingw32-make
